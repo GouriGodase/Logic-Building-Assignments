4d3eb8e136b0431c3319cee66b0b47cc4d992c1b
GouriGodase <gourigodase804@gmail.com>
2025-05-21 12:17:05 +0530

    Add files via upload

---
+++ Assignment  10/Q4.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int MultDigit(int iNo)
+{
+    int iDigit = 0;
+    int iMult = 1;
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit <= 0)
+        {
+            iDigit = 1;
+        }
+        iMult = iMult * iDigit;
+        iNo = iNo / 10;
+
+    }
+    return iMult;
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number:\n");
+    scanf("%d",&iValue);
+
+    iRet = MultDigit(iValue);
+
+    printf("%d",iRet);
+    
+    return 0;
+
+}
