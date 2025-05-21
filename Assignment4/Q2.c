bf0a9f7249013062bf15b027b714209b05fe08b7
GouriGodase <gourigodase804@gmail.com>
2025-05-21 12:08:07 +0530

    Add files via upload

---
+++ Assignment4/Q2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void  FactRev(int iNo)
+{
+    int iCnt = 0;
+    for(iCnt = (iNo/2); iCnt >=1; iCnt--)
+    {
+        if(iNo % iCnt == 0)
+        {
+            printf("%d\n",iCnt);
+        }
+    }
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter number ");
+    scanf("%d", &iValue);
+
+    printf("Factors are :\n");
+    FactRev(iValue);
+
+    return 0;
+}
