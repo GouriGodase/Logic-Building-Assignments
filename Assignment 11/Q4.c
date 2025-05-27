086c96f8c36fe44f0f9247973aa81d4065dfda5b
GouriGodase <gourigodase804@gmail.com>
2025-05-27 20:33:20 +0530

    Add files via upload

---
+++ Assignment 11/Q4.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+ 
+void pattern(int iNo)
+{
+    
+    int iCnt = 0;
+
+    for(iCnt  = 1;iCnt <=iNo; iCnt++)
+    {
+        printf("*\t");
+    }
+    
+
+    
+     for(iCnt  = 1;iCnt <=iNo; iCnt++)
+    {
+        printf("#\t");
+    }
+     for(iCnt  = 1;iCnt <=iNo; iCnt++)
+    {
+        printf("%d\t", iCnt);
+    }
+    printf("\n");
+
+        
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number of  \n");
+    scanf("%d",&iValue);
+
+    pattern(iValue);
+
+    return 0;
+}
