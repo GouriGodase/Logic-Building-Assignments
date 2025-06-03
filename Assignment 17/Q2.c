d3110333d27a4fc2267ec45883767b9f24816757
GouriGodase <gourigodase804@gmail.com>
2025-06-03 12:50:36 +0530

    Add files via upload

---
+++ Assignment 17/Q2.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = iCol; j >= 1; j--)
+        {
+            if(i == j)
+            {
+                printf("#\t",j);
+            }
+            else if(i > j)
+            {
+                printf("@\t", j);
+            }
+            else
+            {
+                printf("*\t",j);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of Rows:");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of Columns:");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
